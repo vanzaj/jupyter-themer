c0c6b24c235bb441b17d76d635f7ec43a8976150
Ivan <vanzaj@gmail.com>
2016-07-14 16:29:10 +0800

    Change to local path

--- jupythemer/jupythemer.py
+++ jupythemer/jupythemer.py
@@ -1,19 +1,20 @@
 from __future__ import print_function
 
-import jupyter
 import os
 import sys
 import argparse
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
-jupyter_dir = os.path.dirname(jupyter.__file__)
-custom_css_filepath = jupyter_dir + '/notebook/static/custom/custom.css'
+
+custom_css_filepath = os.path.join(os.path.expanduser('~'),
+        '.jupyter', 'custom', 'custom.css')
 
 
 def write_to_css(content):
     try:
         with open(custom_css_filepath, 'w') as f:
             f.write(content)
+        print('Written {}'.format(custom_css_filepath))
     except:
         print('Error writing to custom.css')
         sys.exit(1)
@@ -22,20 +23,21 @@ def write_to_css(content):
 def run(args=None):
     if args is None:
         parser = argparse.ArgumentParser(description='Jupyter notebook themer.')
-        parser.add_argument('-c', '--color', required=False, dest='color', default=None, help='color style')
+        parser.add_argument('-c', '--colors', required=False, dest='color', default=None, help='color style')
         parser.add_argument('-l', '--layout', required=False, dest='layout', default=None, help='layout style')
         parser.add_argument('-t', '--typography', required=False, dest='typography', default=None, help='typography style')
         args = parser.parse_args()
 
     if args.color is None and args.layout is None and args.typography is None:
-        print('Jupyter notebook reverted to default style.')
         write_to_css('')
+        print('Jupyter notebook reverted to default style.')
         sys.exit()
 
     content_all = ''
     if args.color is not None:
         try:
-            with open('{}/styles/color/{}.css'.format(current_dir, args.color), 'r') as f_color:
+            fn = '{}/styles/color/{}.css'.format(current_dir, args.color)
+            with open(fn, 'r') as f_color:
                 content_all += f_color.read() + '\n'
         except:
             print('Bad argument passed to --color')
@@ -43,7 +45,8 @@ def run(args=None):
 
     if args.layout is not None:
         try:
-            with open('{}/styles/layout/{}.css'.format(current_dir, args.layout), 'r') as f_layout:
+            fn = '{}/styles/layout/{}.css'.format(current_dir, args.layout)
+            with open(fn, 'r') as f_layout:
                 content_all += f_layout.read() + '\n'
         except:
             print('Bad argument passed to --layout')
@@ -51,7 +54,8 @@ def run(args=None):
 
     if args.typography is not None:
         try:
-            with open('{}/styles/typography/{}.css'.format(current_dir, args.typography), 'r') as f_typography:
+            fn = '{}/styles/typography/{}.css'.format(current_dir, args.typography)
+            with open(fn, 'r') as f_typography:
                 content_all += f_typography.read() + '\n'
         except:
             print('Bad argument passed to --typography')
